ee4e92be0981df825ecd8db16b14b60cb23de12a
Ritik Goyal <ritik.goyal@lendbox.com>
2022-09-15 00:07:40 +0530

    adding bodyparser middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ urlencoded: true }));
 
 app.get('/health', (req, res, next) => {
     res.status(200).json({
